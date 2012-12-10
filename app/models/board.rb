693437c220ec99279523242602cde3e0a473d16b
Ng Guoyou <fauxface+github@gmail.com>
2012-12-10 21:27:51 +0800

    Max discussions for board, prune aged discussions
    Pruning of aged discussions defaults to false

--- app/models/board.rb
+++ app/models/board.rb
@@ -4,4 +4,29 @@ class Board < ActiveRecord::Base
   has_many :posts, dependent: :destroy
   accepts_nested_attributes_for :discussions
   accepts_nested_attributes_for :posts
+  after_update :prune_old_discussions
+  after_update :prune_aged_discussions if APP_CONFIG['prune_by_age']
+  validate :discussions_count_within_bounds
+
+  def discussions_count_within_bounds
+    return if discussions.blank?
+    if discussions.count > APP_CONFIG['max_discussions_in_board']
+      errors.add(:base, "Discussion limit reached. (#{APP_CONFIG['max_discussions_in_board']})")
+    end
+  end
+
+  # By limit
+  def prune_old_discussions
+    while discussions.count > APP_CONFIG['max_discussions_in_board']
+      discussions.first(order: 'last_post_at asc').destroy
+    end
+  end
+
+  # By time
+  def prune_aged_discussions
+    return if discussions.blank?
+    while ((Time.now - (d = discussions.first(order: 'created_at asc'))) > APP_CONFIG['max_discussions_age'])
+      d.destroy
+    end
+  end
 end
