b29d27eeb3fa8ffdc2e69382d1f75ed3f7513ec5
Ng Guoyou <fauxface+github@gmail.com>
2012-12-17 07:05:22 +0800

    Scale poll interval
    Poll interval increases on empty poll (ie. no new posts)

--- app/assets/javascripts/discussions.js
+++ app/assets/javascripts/discussions.js
@@ -1,25 +1,38 @@
 $(function() {
     if ($("#discussion-info").length > 0 && $(".post").length > 0) {
         var poll_interval = $("#discussion-info").attr("data-poll-interval");
-        setInterval(function() { updatePosts(poll_interval/1000); }, poll_interval);
-        setInterval(function() { updateTimer(poll_interval/1000); }, 1000);
+        var poll_attempts = 0;
+        var poll_increment = 1500;
+        var max_increments = 75; // Max is around 30 minutes
+
+        // Set initial update timer
+        $("#discussion-info-updating-in-seconds").html(poll_interval/1000);
+        setTimeout(function() { updatePosts(poll_interval, poll_attempts, poll_increment, max_increments); }, poll_interval);
+        setInterval(function() { updateTimer((poll_interval + poll_attempts * poll_increment)/1000); }, 1000);
     }
 });
 
-function updatePosts(poll_interval) {
+function updatePosts(poll_interval, poll_attempts, poll_increment, max_increments) {
     discussion_id = $("#discussion-info").attr("data-discussion-id");
     after = $(".post").last().attr("data-post-id");
     $.getScript("/posts/poll.js?discussion_id=" + discussion_id + "&after=" + after);
-    $("#discussion-info-updating-in-seconds").html(poll_interval/1000);
+
+    if (after == $(".post").last().attr("data-post-id")) {
+        // No new posts, increase poll interval
+        poll_attempts = Math.min(poll_attempts++, max_increments);
+    } else {
+        poll_attempts = 0;
+    }
+
+    $("#discussion-info-updating-in-seconds").html((poll_interval + poll_attempts * poll_increment)/1000);
+
+    alert(poll_interval + poll_attempts * poll_increment);
+
+    setTimeout(function() { updatePosts(poll_interval, poll_attempts, poll_increment, max_increments); }, poll_interval + poll_attempts * poll_increment);
 }
 
 function updateTimer(poll_interval) {
     $('#discussion-info-updating-in-seconds').each( function() {
-        var count = parseInt($(this).html(), 10);
-        if (count > 0) {
-            $(this).html(count - 1);
-        } else {
-            $(this).html(poll_interval);
-        }
+        $(this).html(parseInt($(this).html(), 10) - 1);
     });
 }
