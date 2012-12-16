627463e5ccd05c1472851dcd6be81fd0aabdc419
Ng Guoyou <fauxface+github@gmail.com>
2012-12-17 07:15:23 +0800

    Fix really broken poll interval

--- app/assets/javascripts/discussions.js
+++ app/assets/javascripts/discussions.js
@@ -1,14 +1,14 @@
 $(function() {
     if ($("#discussion-info").length > 0 && $(".post").length > 0) {
-        var poll_interval = $("#discussion-info").attr("data-poll-interval");
+        var poll_interval = parseInt($("#discussion-info").attr("data-poll-interval"), 10);
         var poll_attempts = 0;
-        var poll_increment = 1500;
+        var poll_increment = 15000;
         var max_increments = 75; // Max is around 30 minutes
 
         // Set initial update timer
         $("#discussion-info-updating-in-seconds").html(poll_interval/1000);
         setTimeout(function() { updatePosts(poll_interval, poll_attempts, poll_increment, max_increments); }, poll_interval);
-        setInterval(function() { updateTimer((poll_interval + poll_attempts * poll_increment)/1000); }, 1000);
+        setInterval(updateTimer, 1000);
     }
 });
 
@@ -19,16 +19,16 @@ function updatePosts(poll_interval, poll_attempts, poll_increment, max_increment
 
     if (after == $(".post").last().attr("data-post-id")) {
         // No new posts, increase poll interval
-        poll_attempts = Math.min(poll_attempts++, max_increments);
+        poll_attempts = Math.min(poll_attempts + 1, max_increments);
     } else {
         poll_attempts = 0;
     }
 
-    $("#discussion-info-updating-in-seconds").html((poll_interval + poll_attempts * poll_increment)/1000);
-    setTimeout(function() { updatePosts(poll_interval, poll_attempts, poll_increment, max_increments); }, poll_interval + poll_attempts * poll_increment);
+    $("#discussion-info-updating-in-seconds").html((poll_interval + (poll_attempts * poll_increment))/1000);
+    setTimeout(function() { updatePosts(poll_interval, poll_attempts, poll_increment, max_increments); }, poll_interval + (poll_attempts * poll_increment));
 }
 
-function updateTimer(poll_interval) {
+function updateTimer() {
     $('#discussion-info-updating-in-seconds').each( function() {
         $(this).html(parseInt($(this).html(), 10) - 1);
     });
