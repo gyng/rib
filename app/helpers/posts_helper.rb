be17ce6742c11ce670cffe45743ea4e4be34dc03
Ng Guoyou <fauxface+github@gmail.com>
2013-04-26 23:43:10 +0800

    Better YouTube embed formatting

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -5,10 +5,11 @@ module PostsHelper
       youtube_id = $1
     else
       # Regex modified from # http://stackoverflow.com/questions/3452546/javascript-regex-how-to-get-youtube-video-id-from-url/4811367#4811367
-      youtube_url[/\S*((v\/)|(embed\/)|(watch\?))\??v?=?([^\&\? \n]*)/]
+      youtube_url[/\S*youtu\S*((v\/)|(embed\/)|(watch\?))\??v?=?([^\&\? \n]*)/]
       youtube_id = $5
     end
 
-    %Q{<iframe title="YouTube video player" width="610" height="371" src="http://www.youtube.com/embed/#{ youtube_id }" frameborder="0" allowfullscreen></iframe>}
+    return nil if youtube_id.nil?
+    %Q{<iframe title="YouTube video player" width="100%" height="380px" src="http://www.youtube.com/embed/#{ youtube_id }" frameborder="0" allowfullscreen></iframe>}
   end
 end
